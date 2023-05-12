728735ca6c8afba0cc9b03814c5fa1053ff255b0
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2023-05-12 15:47:17 -0300

    Add files

---
+++ Ecommerce/Repositories/IPedidoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repositories
+{
+    public interface IPedidoRepository
+    {
+        void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+        void Remover(int id);
+        Pedido ObterPorId(int id);
+        List<Pedido> ObterTodos();
+        List<Pedido> ObterPorCliente(string cliente);
+        List<Pedido> ObterPorStatus(string status);
+        List<Pedido> ObterPorData(DateTime data);
+    }
+}
